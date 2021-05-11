6c6b55323f24c9e96c16583562bf8b652d507d47
longen0707 <297211982@qq.com>
2021-05-11 16:54:52 +0800

    第二次提交

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace TestGit
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("dasdas");
+        }
     }
 }
